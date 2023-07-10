d795881c57c62c504c27755ef44f8037047008a3
Vitor Samuel <108810741+VitorSmelo021@users.noreply.github.com>
2023-07-10 12:34:01 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+const os = require('os')
+const http = require('http')
+const url = require('url')
+
+const PORT = 5000
+
+// =====================================Atv-1==================================
+
+// const objeto = [
+//     {nome: (os.hostname())},
+//     {memoria:(os.freemem())},
+//     {diretorio:(os.homedir())},
+// ]
+//
+// const server = http.createServer((req, res,) =>{
+//     if(req.url === '/info'){
+//         res.setHeader('Content-Type', 'application/json')
+//         res.end()
+//     }})
+// const json = JSON.stringify(objeto)
+// console.log(json)
+//
+//
+// =====================================Atv-2==================================
+
+
+// const address = 'https://al.senai.br/cursos/tecnico-em-informatica-para-internet/planodecurso.pdf';
+// const parseUrl = new url.URL(address)
+
+// const objeto = [
+//     {
+//         root: (parseUrl.search),
+//         dir: (parseUrl.href),
+//         base:(parseUrl.),
+//         ext: (parseUrl.hash)
+//     }
+// ]
+
+// console.log(objeto)
+
+// =====================================Atv-3==================================
+
+ const newUrl[
+    {
+        protocol:,
+    }
+ ]
+
+
+// ============================================================================
+
+
+// server.listen(PORT, ()=>{
+//     console.log(`Servidor rodando na porta ${PORT}`)
+// })
